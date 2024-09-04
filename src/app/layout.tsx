e276111c8e778765a0c65ebbd05ce41fe36bf447
JoseAE <107892696+Jose-AE@users.noreply.github.com>
2024-09-04 15:06:27 -0600

    Add script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Rubik } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Links from "@/components/Links";
@@ -20,6 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2934132820353961"
+          crossOrigin="anonymous"
+        ></Script>
         <Analytics />
         <Navbar>{children}</Navbar>
         <Links />
